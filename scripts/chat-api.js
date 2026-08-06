f1ed0dbbf1c85b75b0ccf4559e5ff4f3d77053cd
YoavLippi <131270199+YoavLippi@users.noreply.github.com>
2026-08-06 21:41:30 +0200

    Changed URL to free render server

--- scripts/chat-api.js
+++ scripts/chat-api.js
@@ -23,7 +23,7 @@ async function SendMessage() {
 
         messages.push({ role: "user", content: userText });
 
-        const res = await fetch("http://localhost:3000/api/chat", {
+        const res = await fetch("https://s-a-f-e-ai.onrender.com", {
             method: 'POST',
             headers: {
                 "Content-Type": 'application/json',
